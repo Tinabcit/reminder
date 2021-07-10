3a29662202a5f8b83b503bdcbc3acacc5a7772f3
ACIT 2520 reminder <tshabestari@my.bcit.ca>
2021-07-09 22:17:29 -0700

    Changed and putting in the node system.

---
+++ controllers/reminder_controller.js
@@ -0,0 +1,65 @@
+let database = require("../database");
+
+let remindersController = {
+  list: (req, res) => {
+    res.render("reminder/index", { reminders: database.cindy.reminders });
+  },
+
+  new: (req, res) => {
+    res.render("reminder/create");
+  },
+
+  listOne: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    if (searchResult != undefined) {
+      res.render("reminder/single-reminder", { reminderItem: searchResult });
+    } else {
+      res.render("reminder/index", { reminders: database.cindy.reminders });
+    }
+  },
+
+  create: (req, res) => {
+    let reminder = {
+      id: database.cindy.reminders.length + 1,
+      title: req.body.title,
+      description: req.body.description,
+      completed: false,
+    };
+    database.cindy.reminders.push(reminder);
+    res.redirect("/reminders");
+  },
+
+  edit: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    res.render("reminder/edit", { reminderItem: searchResult });
+  },
+
+  update: (req, res) => {
+      reminderupdate.update(req.params.id,function(err){
+          if(err){
+              return console.log(err)
+            }
+          req.flash('update','the item has been update')
+          res.redirect('/reminder/update')
+      };
+    
+  },
+
+  delete: (req, res) => {
+    remindersdelete.delete(req.params.id, function(err){
+        if(err){
+            return console.log(err);
+        } 
+        req.flash('success','the item is deleted')
+        res.redirect('/reminder/delete')
+    };
+},
+
+module.exports = remindersController;
+
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
+const app = express();
+const reminderController = require("./controllers/reminder_controller");
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended:false}));
+app.use(ejsLayouts);
+app.set("view engine","ejs");
+
+// Case 1: User goes to localhost:8081/ -> Homepage/Marketing Page
+app.get("/",function(req,res) {
+    res.send("Welcome to our landing page. Marketing content goes here");
+})
+// facebook.com/cindy/newsfeed
+
+// Case 2: User goes to localhost:8081/reminder -> Show a list of reminders
+
+app.get("/reminder", reminderController.list)
+
+// Case 3: User goes to localhost:8081/reminder/new -> Show a CREATE REMINDER PAGE
+app.get("/reminder/new", reminderController.new)
+
+// Case 4: User SENDS NEW REMINDER DATA TO US ((CREATING A REMINDER))
+
+app.post("/reminder", reminderController.create)
+
+//Case 5: User wants to SEE an individual reminder
+
+app.get("/reminder/:id",reminderController.listOne)
+
+// Case 6: User wants to EDIT an individual reminder
+
+app.get("/reminder/edit/id:",reminderController.edit)
+
+// Case 7: User wants to UPDATE by clicking the UPDATE BUTTON from Case 6, and excepts their reminder to be updated
+
+app.post("/reminder/edit/id:", reminderController.update)
+
+// Case 8: User clicks the DELETE BUTTON and we expect the reminder to be deleted.
+
+app.post("/reminder/delete/id:", reminderController.delete)
+
+// localhost:8081
+app.listen(8081);
