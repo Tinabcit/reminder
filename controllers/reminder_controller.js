472725a01dfb29ffb0c26f0a6a80bdc1e67c8622
Tina <tshabestari@my.bcit.ca>
2021-07-23 11:50:12 -0700

    Changed the format so the nodemon runs through it.

--- controllers/reminder_controller.js
+++ controllers/reminder_controller.js
@@ -16,12 +16,21 @@ let remindersController = {
   new: (req, res) => {
     res.render("reminder/create");
   },
-
-  listOne: (req, res) => {
+  create: (req, res) => {
+    let reminder = {
+      id: database.cindy.reminders.length + 1,
+      title: req.body.title,
+      description: req.body.description,
+      completed: false,
+    }
+    database.cindy.reminders.push(reminder);
+    res.redirect("/reminders");
+  },
+  listOne:function(req, res){
     let reminderToFind = req.params.id;
     let searchResult = database.cindy.reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
-    });
+    })
     if (searchResult != undefined) {
       res.render("reminder/single-reminder", { reminderItem: searchResult });
     } else {
@@ -29,16 +38,4 @@ let remindersController = {
     }
   }
 }
-
-  create: (req, res) => {
-    let reminder = {
-      id: database.cindy.reminders.length + 1,
-      title: req.body.title,
-      description: req.body.description,
-      completed: false,
-    };
-    database.cindy.reminders.push(reminder);
-    res.redirect("/reminders");
-  },
-
 module.exports = remindersController;
