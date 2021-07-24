fb69a8ba0de8b5b4d862b6fae347e95d81256c05
Tina <tshabestari@my.bcit.ca>
2021-07-23 17:51:31 -0700

    Add the database.ejs for the login user and user accounts.

--- database.js
+++ database.js
@@ -1,10 +1,62 @@
 let Database = {
     cindy: {
-        reminders: []
+        reminders: [
+            {
+                id: 1,
+                title:" To do list one item",
+                description: " First Task to be done",
+                completed: false,
+            },
+            {
+                id: 2,
+                title:" To do list two item",
+                description: " Second Task to be done",
+                completed: false,
+            },
+            {
+                id: 3,
+                title:" To do list third item",
+                description: " Third Task to be done",
+                completed: false,
+            },
+            {
+                id: 4,
+                title:" To do list Fourth item",
+                description: " Fourth Task to be done",
+                completed: false,
+            },
+        ],
     },
     alex: {
-        reminders:[]
-    }
+        reminders:[
+            {
+                id: 1,
+                title:" To do list one item",
+                description: " First Task to be done",
+                completed: false,
+            },
+            {
+                id: 2,
+                title:" To do list two item",
+                description: " Second Task to be done",
+                completed: false,
+            },
+            {
+                id: 3,
+                title:" To do list third item",
+                description: " Third Task to be done",
+                completed: false,
+            },
+            {
+                id: 4,
+                title:" To do list Fourth item",
+                description: " Fourth Task to be done",
+                completed: false,
+            },
+        ],
+    },
 }
+
+Database.cindy.reminders.push({})
 
 module.exports= Database;
